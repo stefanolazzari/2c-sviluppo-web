d4d008ca77c23c248bed913a5b02c4b3af0c967c
stefanolazzari <stefanolazzari79@libero.it>
2024-12-19 10:59:40 +0000

    aggiungi elemento + icona cestino

--- aggiungiElemento.js
+++ aggiungiElemento.js
@@ -1,3 +1,27 @@
 //Seleziona il bottone di aggiunta
 
 const bottoneAggiungi = document.getElementById('bottoneAggiungi');
+const bottoneTogli = document.getElementById('bottoneTogli');
+const lista = document.getElementById('miaLista');
+
+console.log(lista.children.length);
+
+
+bottoneAggiungi.addEventListener('click', function() {
+
+    const nuovoElemento = document.createElement('li');
+    const userInput = prompt("inserisci un testo");
+    const iconaCestino = document.createElement('span');
+
+    nuovoElemento.textContent = userInput;
+    iconaCestino.textContent = " 🗑️"
+    iconaCestino.style.cursor = "pointer";
+
+    nuovoElemento.appendChild(iconaCestino);
+    lista.appendChild(nuovoElemento);
+
+    iconaCestino.addEventListener('click', function(){
+            nuovoElemento.remove();
+    })
+
+})

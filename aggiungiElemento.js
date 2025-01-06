ad48ac2ae162239486cdada176137bcac03bd6a1
stefanolazzari <stefanolazzari79@libero.it>
2025-01-06 10:39:41 +0000

    centratura, bottone svuota

--- aggiungiElemento.js
+++ aggiungiElemento.js
@@ -1,13 +1,24 @@
 //Seleziona il bottone di aggiunta
 
 const bottoneAggiungi = document.getElementById('bottoneAggiungi');
-const bottoneTogli = document.getElementById('bottoneTogli');
+//const bottoneTogli = document.getElementById('bottoneTogli');
 const lista = document.getElementById('miaLista');
+const bottoneSvuota = document.getElementById('svuota');
 
 console.log(lista.children.length);
 
 
-bottoneAggiungi.addEventListener('click', function() {
+
+function controllaSeListaVuota(){
+    if (condition) {
+        
+    } else {
+        
+    }
+
+}
+
+bottoneAggiungi.addEventListener('click', function () {
 
     const nuovoElemento = document.createElement('li');
     const userInput = prompt("inserisci un testo");
@@ -20,8 +31,20 @@ bottoneAggiungi.addEventListener('click', function() {
     nuovoElemento.appendChild(iconaCestino);
     lista.appendChild(nuovoElemento);
 
-    iconaCestino.addEventListener('click', function(){
-            nuovoElemento.remove();
+    iconaCestino.addEventListener('click', function () {
+        nuovoElemento.remove();
     })
 
+});
+
+bottoneSvuota.addEventListener('click', function () {
+    const confermaCanc = confirm("Sei sicuro di voler svuotare la lista?");
+    if (confermaCanc) {
+
+        if (lista.children.length > 0) {
+            lista.innerHTML = '';
+        }
+    }
 })
+
+

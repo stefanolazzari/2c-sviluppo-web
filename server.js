15b1e768c0288fb3a31c002a9b6c3e160753371d
stefanolazzari <stefanolazzari79@libero.it>
2025-02-03 20:35:56 +0000

    lista.json

--- server.js
+++ server.js
@@ -11,7 +11,31 @@ app.use(cors());
 // Servire la cartella pubblica
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
+// Percorso del file JSON
+const filePath = path.join(__dirname, 'lista.json');
+
+// API per salvare la lista in un file JSON
+app.post('/salva-lista', (req, res) => {
+  const lista = req.body.lista;
+
+  if (!Array.isArray(lista)) {
+    return res.status(400).json({ message: 'Dati non validi' });
+  }
+
+  fs.writeFile(filePath, JSON.stringify(lista, null, 2), (err) => {
+    if (err) {
+      return res.status(500).json({ message: 'Errore nel salvataggio' });
+    }
+    res.json({ message: 'Lista salvata con successo!' });
+  });
+});
+
+
 // Avvia il server
 app.listen(PORT, () => {
     console.log(`Server avviato su http://localhost:${PORT}`);
 });
+
+

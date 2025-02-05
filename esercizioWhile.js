6dfbb02b673f8eab5530337afdbdd89084dc596e
stefanolazzari <stefanolazzari79@libero.it>
2025-02-05 09:28:14 +0000

    esercizio ripeti

---
+++ esercizioWhile.js
@@ -0,0 +1,23 @@
+let ripeti = 5;
+let i = 0;
+
+
+//stampa la variabile "ripeti" per il numero di volte pari al suo valore
+while (i < ripeti) {
+    console.log(ripeti);
+    i++
+}
+
+
+// crea conto alla rovescia #1
+while (i < ripeti) {
+    console.log(ripeti);
+    ripeti--;
+}
+
+
+//conto alla rovescia #2
+while (i < ripeti) {
+    console.log(ripeti-i);
+    i++
+}
